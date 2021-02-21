342543e0882555add8cd0bacd8d88ebe0f0695ed
Eunsook Shin <eunsooks@usc.edu>
2021-02-20 23:41:19 -0800

    edited hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,8 +3,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "incoming change from VS";
     std::cout << "Hello World!!";
-    std::cout << "incoming change from remote";
+    std::cout << "incoming change from VS";
     return 0;
 }
